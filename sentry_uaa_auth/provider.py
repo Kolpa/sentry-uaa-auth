922e45db409c59c902330c86e66d6a743a25d4ab
Kolya Opahle <kolyaopahle@gmail.com>
2018-12-28 17:06:23 +0100

    fixed the exchange_token call

--- sentry_uaa_auth/provider.py
+++ sentry_uaa_auth/provider.py
@@ -3,9 +3,27 @@ from __future__ import absolute_import, print_function
 from sentry.auth.exceptions import IdentityNotValid
 from sentry.auth.providers.oauth2 import OAuth2Provider, OAuth2Login, OAuth2Callback
 
+from six.moves.urllib.parse import parse_qsl
+from sentry.http import safe_urlopen, safe_urlread
+from sentry.utils import json
+from sentry.utils.http import absolute_uri
+
 from .constants import (
     AUTHORIZE_URL, ACCESS_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, SCOPE
 )
+
+
+class FixedOAuth2Callback(OAuth2Callback):
+    def exchange_token(self, request, helper, code):
+        # TODO: this needs the auth yet
+        data = self.get_token_params(
+            code=code,
+            redirect_uri=absolute_uri(helper.get_redirect_url()),
+        )
+        req = safe_urlopen(self.access_token_url, data=data)
+        body = safe_urlread(req)
+        print(body)
+        return json.loads(body)
 
 
 class CFUaaProvider(OAuth2Provider):
@@ -23,7 +41,7 @@ class CFUaaProvider(OAuth2Provider):
                 client_id=self.client_id,
                 scope=self.scope,
             ),
-            OAuth2Callback(
+            FixedOAuth2Callback(
                 access_token_url=self.access_token_url,
                 client_id=self.client_id,
                 client_secret=self.client_secret,
