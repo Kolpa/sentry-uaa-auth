42a57da55e212148f9821b50b5004ca61936af72
Kolya Opahle <kolyaopahle@gmail.com>
2018-12-28 15:46:19 +0100

    fixed use of getattr

--- sentry_uaa_auth/constants.py
+++ sentry_uaa_auth/constants.py
@@ -4,13 +4,13 @@ from django.conf import settings
 
 CONF_OBJ = getattr(settings, 'UAA_CONF', None)
 
-CLIENT_ID = getattr(CONF_OBJ, 'clientid', None)
+CLIENT_ID = CONF_OBJ.get('clientid', None)
 
-CLIENT_SECRET = getattr(CONF_OBJ, 'clientsecret', None)
+CLIENT_SECRET = CONF_OBJ.get('clientsecret', None)
 
-BASE_DOMAIN = getattr(CONF_OBJ, 'url', None)
+BASE_DOMAIN = CONF_OBJ.get('url', None)
 
-APP_NAME = getattr(CONF_OBJ, 'xsappname', None)
+APP_NAME = CONF_OBJ.get('xsappname', None)
 
 SCOPE = APP_NAME + ".login"
 
