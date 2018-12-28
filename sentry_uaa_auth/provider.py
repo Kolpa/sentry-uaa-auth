5f3cdc86be48c53db947cb3b917c31b475e203fa
Kolya Opahle <kolyaopahle@gmail.com>
2018-12-28 16:26:57 +0100

    fixed login pipeline

--- sentry_uaa_auth/provider.py
+++ sentry_uaa_auth/provider.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function
 
 from sentry.auth.exceptions import IdentityNotValid
-from sentry.auth.providers.oauth2 import OAuth2Provider
+from sentry.auth.providers.oauth2 import OAuth2Provider, OAuth2Login, OAuth2Callback
 
 from .constants import (
     AUTHORIZE_URL, ACCESS_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, SCOPE
@@ -14,6 +14,21 @@ class CFUaaProvider(OAuth2Provider):
     name = 'UAA'
     client_id = CLIENT_ID
     client_secret = CLIENT_SECRET
+    scope = SCOPE
+
+    def get_auth_pipeline(self):
+        return [
+            OAuth2Login(
+                authorize_url=self.authorize_url,
+                client_id=self.client_id,
+                scope=self.scope,
+            ),
+            OAuth2Callback(
+                access_token_url=self.access_token_url,
+                client_id=self.client_id,
+                client_secret=self.client_secret,
+            ),
+        ]
 
     def get_refresh_token_url(self):
         return ACCESS_TOKEN_URL
